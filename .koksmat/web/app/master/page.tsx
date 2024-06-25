7ca9f9dd22c7628c4c0986b6d90b26b0ca1d0012
Niels Gregers Johansen <niels@timeboxer.dk>
2024-06-25 14:16:44 +0200

    more

--- .koksmat/web/app/master/page.tsx
+++ .koksmat/web/app/master/page.tsx
@@ -2,12 +2,13 @@
 
 import { useRouter } from "next/navigation";
 import { APPNAME } from "../global";
+import LiveNexi from "@/components/livenexi";
 
 export default function Page() {
   const router = useRouter();
   return (
     <div className="space-x-2 h-[90vh] place-content-center ">
-      <div className="text-4xl">Welcome to {APPNAME}</div>
+      <LiveNexi />
     </div>
   );
 }
